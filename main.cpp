1cf9566e501982139b55a43f9f5b3a302281910a
renan <renan@renan-pc>
2016-06-21 15:48:45 -0400

    CLI interface.

--- main.cpp
+++ main.cpp
@@ -7,7 +7,6 @@
 #include "tsp2opt.h"
 #include "tsptabu.h"
 
-
 void test(std::string filename)
 {
     tsp::TSPLibData data;
@@ -33,11 +32,26 @@ void testRun()
     test("att532.tsp");
 }
 
+void printHelp(std::string executableName)
+{
+    std::cout << "Usage:\n"
+              << "\t" << executableName << " [option] filename\n"
+              << "Options:\n"
+              << "\t--help              : Prints this help message.\n"
+              << "\t--test              : Runs the Canonnical Tests from TSPLIB.\n"
+              << "\t--only-constructive : Runs only the Nearest Neighbour construtive greedy heuristic.\n"
+              << "\t--localsearch       : Runs the Nearest Neighbour with 2-Opt local search.\n"
+              << "\t--grasp             : Runs the Nearest Neighbour with 2-Opt using GRASP metaheuristic.\n"
+              << "\t--tabu              : Runs the Nearest Neighbour with 2-Opt using Tabu Search metaheuristic.\n"
+              << std::endl;
+}
+
 int main(int argc, char *argv[])
 {
     if (argc <= 1)
     {
-        std::cout << "Inform filename." << std::endl;
+        std::cout << "Error: Please inform options and filename." << std::endl;
+        printHelp(argv[0]);
         return 1;
     }
 
@@ -49,24 +63,73 @@ int main(int argc, char *argv[])
         return 0;
     }
 
-    tsp::TSPLibData data;
-    std::ifstream file(option);
-    data.load(file);
-
-    tsp::TSPGrasp grasp(data, 50);
-    tsp::TSPTour tourGrasp = grasp.run();
-
-    if ( tourGrasp.isValid() )
+    if (option == "--help")
     {
-        tourGrasp.print(std::cout);
+        printHelp(argv[0]);
+        return 0;
     }
 
-    tsp::TSPTabu tabu(data, 50, 5, 10);
-    tsp::TSPTour tourTabu = tabu.run();
+    tsp::TSPLibData data;
+    std::ifstream file(argv[2]);
 
-    if ( tourTabu.isValid() )
+    if (!file.is_open())
     {
-        tourTabu.print(std::cout);
+        std::cout << "Error: cannot open file." << std::endl;
+        printHelp(argv[0]);
+        return 0;
+    }
+
+    data.load(file);
+
+    if (option == "--only-constructive")
+    {
+        tsp::TSPNearestNeighborConstruct nn(data);
+        tsp::TSPTour tour = nn.run();
+
+        if (tour.isValid())
+        {
+            tour.print(std::cout);
+        }
+        return 0;
+    }
+
+    if (option == "--localsearch")
+    {
+        tsp::TSPNearestNeighborConstruct nn(data);
+        tsp::TSPTour tour = nn.run();
+
+        tsp::TSP2opt local(data, tour);
+        tour = local.run();
+
+        if (tour.isValid())
+        {
+            tour.print(std::cout);
+        }
+        return 0;
+    }
+
+    if (option == "--grasp")
+    {
+        tsp::TSPGrasp grasp(data, 100);
+        tsp::TSPTour tourGrasp = grasp.run();
+
+        if ( tourGrasp.isValid() )
+        {
+            tourGrasp.print(std::cout);
+        }
+        return 0;
+    }
+
+    if (option == "--tabu")
+    {
+        tsp::TSPTabu tabu(data, 100, 10, 10);
+        tsp::TSPTour tourTabu = tabu.run();
+
+        if ( tourTabu.isValid() )
+        {
+            tourTabu.print(std::cout);
+        }
+        return 0;
     }
 
     return 0;
